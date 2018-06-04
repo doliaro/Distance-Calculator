7b932243af7fb45429680605c639af08e5a7336c
Dom Oliaro <domoliaro@gmail.com>
2018-06-04 00:32:22 -0600

    fix

--- app/models/distance.rb
+++ app/models/distance.rb
@@ -3,22 +3,5 @@ class Distance < ApplicationRecord
     geocoded_by :destination_address
     reverse_geocoded_by :latitude, :longitude
     after_validation :geocode
-    # attr_accessor :distance_length
-
-    # distance_length = Geocoder::Calculations.distance_between(
-    #                   Geocoder.coordinates(Distance.start_address),
-    #                   Geocoder.coordinates(Distance.destination_address))
 end
 
-# 1, miles, kilometers, drive_time, walk_time
-# metrics
-# metric.rb
-# metrics_controller.rb
-class Metric < ApplicationRecord
-end
-
-# distance_metrics
-# distance_metric.rb
-# distance_metrics_controller.rb
-class DistanceMetric < ApplicationRecord
-end
